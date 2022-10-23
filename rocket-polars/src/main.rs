2d97415893df6ffbf3fb245e687feddfbdad9aea
colin99d <colin99delahunty@gmail.com>
2022-10-23 19:30:25 +0200

    Simplified main.rs

--- rocket-polars/src/main.rs
+++ rocket-polars/src/main.rs
@@ -10,29 +10,24 @@ pub fn csv_to_df(path: &str) -> PolarsResult<DataFrame> {
         .with_row_count("Id", None)
 }
 
-pub fn df_to_json(df: &DataFrame) -> Result<Value, String> {
+pub fn df_to_json(df: &DataFrame) -> Value {
     let mut buffer = Vec::new();
 
-    if let Err(_) = JsonWriter::new(&mut buffer)
+    JsonWriter::new(&mut buffer)
         .with_json_format(JsonFormat::Json)
         .finish(&mut df.clone())
-    {
-        return Err("Could not write the dataframe to json".to_string());
-    }
+        .unwrap();
 
-    match String::from_utf8(buffer) {
-        Err(_) => Err("Could not convert the buffer to a string".to_string()),
-        Ok(json) => match serde_json::from_str(&json) {
-            Err(_) => Err("Could not convert the string to json".to_string()),
-            Ok(json) => Ok(json),
-        },
-    }
+    let json_string = String::from_utf8(buffer).unwrap();
+    serde_json::from_str(&json_string).unwrap()
 }
+
+
 #[get("/")]
 pub async fn get_data() -> Value {
     let raw_df = csv_to_df("../data.csv").unwrap();
     let df = raw_df.slice(0, 50000);
-    df_to_json(&df).unwrap()
+    df_to_json(&df)
 }
 
 
